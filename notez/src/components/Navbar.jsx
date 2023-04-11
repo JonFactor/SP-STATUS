378aca37b5ef16dc223c4b3d142afdfb1f8f5a95
JonFactor <Jon.Factor2@gmail.com>
2023-04-11 15:58:28 -0400

    222

---
+++ notez/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' w-full flex flex-row'>
+      <link href='/'> BOARD </link>
+      <link href='/'> BOARD </link>
+      <link href='/'> BOARD </link>
+      <link href='/'> BOARD </link>
+      <link href='/'> BOARD </link>
+    </div>
+  )
+}
+
+export default Navbar
