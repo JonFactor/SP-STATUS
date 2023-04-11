5513242b5816db9acbf862842fa9389433b35816
JonFactor <Jon.Factor2@gmail.com>
2023-04-11 10:59:23 -0400

    222

---
+++ notez/src/components/editNoteModal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const editNoteModal = (parent) => {
+    const main = document.createElement("div")
+    main.style.backgroundColor = parent.style.backgroundColor
+    main.className = " w-96 h-96 absolute bg-black"
+
+}
+
+export default editNoteModal
