5030ca3a23be2f3c8a330cee7cad086b71adc363
JonFactor <Jon.Factor2@gmail.com>
2023-05-29 21:50:14 -0400

    432

---
+++ parkerMarker/frontend/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Home from './views/home';
+import MainWrapper from './layouts/MainWrapper';
+import Login from './views/login';
+import PrivateRoute from './layouts/PrivateRoute';
+import Logout from './views/logout';
+import Private from './views/private';
+import Register from './views/register';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <MainWrapper>
+                    <Route
+                        path="/private"
+                        element={
+                            <PrivateRoute>
+                                <Private />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/logout" element={<Logout />} />
+            </MainWrapper>
+        </BrowserRouter>
+    );
+}
+
+export default App;
